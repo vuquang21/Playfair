c453af0be5ee6da618e37dae0fafc95aba0cb774
Quang <qvu2k1@gmail.com>
2021-11-13 19:40:07 +0700

    playfair_v0.1

---
+++ Playfair/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace Playfair
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbKey = new System.Windows.Forms.TextBox();
+            this.rtbPlaintext = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.rtbCiphtertext = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnEncryp = new System.Windows.Forms.Button();
+            this.btnDecryp = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(82, 188);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(194, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Short version of the Playfair Key";
+            // 
+            // tbKey
+            // 
+            this.tbKey.Location = new System.Drawing.Point(132, 216);
+            this.tbKey.Name = "tbKey";
+            this.tbKey.Size = new System.Drawing.Size(398, 22);
+            this.tbKey.TabIndex = 2;
+            // 
+            // rtbPlaintext
+            // 
+            this.rtbPlaintext.Location = new System.Drawing.Point(132, 40);
+            this.rtbPlaintext.Name = "rtbPlaintext";
+            this.rtbPlaintext.Size = new System.Drawing.Size(398, 125);
+            this.rtbPlaintext.TabIndex = 3;
+            this.rtbPlaintext.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(82, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Plaintext";
+            // 
+            // rtbCiphtertext
+            // 
+            this.rtbCiphtertext.Location = new System.Drawing.Point(132, 354);
+            this.rtbCiphtertext.Name = "rtbCiphtertext";
+            this.rtbCiphtertext.ReadOnly = true;
+            this.rtbCiphtertext.Size = new System.Drawing.Size(398, 125);
+            this.rtbCiphtertext.TabIndex = 3;
+            this.rtbCiphtertext.Text = "";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(82, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Plaintext";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(82, 314);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 16);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Ciphertext";
+            // 
+            // btnEncryp
+            // 
+            this.btnEncryp.Location = new System.Drawing.Point(271, 264);
+            this.btnEncryp.Name = "btnEncryp";
+            this.btnEncryp.Size = new System.Drawing.Size(118, 40);
+            this.btnEncryp.TabIndex = 5;
+            this.btnEncryp.Text = "Encryption";
+            this.btnEncryp.UseVisualStyleBackColor = true;
+            this.btnEncryp.Click += new System.EventHandler(this.btnEncryp_Click);
+            // 
+            // btnDecryp
+            // 
+            this.btnDecryp.Location = new System.Drawing.Point(412, 264);
+            this.btnDecryp.Name = "btnDecryp";
+            this.btnDecryp.Size = new System.Drawing.Size(118, 40);
+            this.btnDecryp.TabIndex = 5;
+            this.btnDecryp.Text = "Decryption";
+            this.btnDecryp.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(628, 547);
+            this.Controls.Add(this.btnDecryp);
+            this.Controls.Add(this.btnEncryp);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.rtbCiphtertext);
+            this.Controls.Add(this.rtbPlaintext);
+            this.Controls.Add(this.tbKey);
+            this.Controls.Add(this.label2);
+            this.Name = "Form1";
+            this.Text = "Playfair";
+            this.Enter += new System.EventHandler(this.btnEncryp_Click);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbKey;
+        private System.Windows.Forms.RichTextBox rtbPlaintext;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox rtbCiphtertext;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnEncryp;
+        private System.Windows.Forms.Button btnDecryp;
+    }
+}
+
---
+++ Playfair/Form1.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Playfair
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+
+        private void Form1_Load(object sender, EventArgs e, List<char> arrPlaintext, KeyEventArgs key)
+        {
+
+        }
+
+        public void KeyAddAlpa(StringBuilder _stringKey, char[] alpa)
+        {
+            int flag = 0;
+            for (int i = 0; i < alpa.Length; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (_stringKey[j] == alpa[i])
+                    {
+                        flag = 1;
+                        break;
+                    }
+                }
+                if (flag == 0)
+                {
+                    _stringKey.Append(alpa[i]);
+                }
+                else
+                {
+                    flag = 0;
+                }
+            }
+
+        }
+        public void isCharacter(StringBuilder _stringPlaintext, int ElementOfPlaintext, char[,] matrix, List<int> arr)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (ElementOfPlaintext < _stringPlaintext.Length)
+                    {
+                        if (_stringPlaintext[ElementOfPlaintext] == matrix[i, j])
+                        {
+                            arr.Add(i);
+                            arr.Add(j);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        private void btnEncryp_Click(object sender, EventArgs e)
+        {
+
+            var alpa = new char[25] {
+                    'A',
+                    'B',
+                    'C',
+                    'D',
+                    'E',
+                    'F',
+                    'G',
+                    'H',
+                    'I',
+                    'K',
+                    'L',
+                    'M',
+                    'N',
+                    'O',
+                    'P',
+                    'Q',
+                    'R',
+                    'S',
+                    'T',
+                    'U',
+                    'V',
+                    'W',
+                    'X',
+                    'Y',
+                    'Z'
+            };
+            var plaintext = rtbPlaintext.Text.Trim();
+            var key = tbKey.Text.Trim();
+            plaintext = plaintext.ToUpper();
+            key = key.ToUpper();
+
+
+            // kiểm tra plaintext và key rỗng
+            if (plaintext == String.Empty || key == String.Empty)
+            {
+                MessageBox.Show("Message: Plaintext or key is empty!!!");
+            }
+            else
+            {
+                StringBuilder stringBuilder = new StringBuilder(key.ToLower());
+                for (int i = 0; i < key.Length; i++)
+                {
+                    if (key[i] == 'J')
+                    {
+                        MessageBox.Show("Message: Please do not enter character 'J'!!!");
+                        stringBuilder.Remove(i, 1);
+                        break;
+                    }
+                }
+                tbKey.Text = stringBuilder.ToString();
+            }
+
+            // xoá những kí tự và khoảng trắng trong key và thay j thành i
+            var charsToRemove = new string[] { "@", ",", ".", ";", "'", " " };
+            foreach (var c in charsToRemove)
+            {
+                key = key.Replace(c, string.Empty);
+                plaintext = plaintext.Replace(c, string.Empty);
+            }
+
+
+            // một cặp có 2 kí tự giống nhau thì thay kí tự sau bằng x
+            StringBuilder _stringPlaintext = new StringBuilder(plaintext);
+            if (plaintext.Length % 2 == 0)
+            {
+                for (int i = 0; ((i < _stringPlaintext.Length) && ((i + 1) < _stringPlaintext.Length)); i += 2)
+                {
+
+                    if (_stringPlaintext[i] == _stringPlaintext[i + 1])
+                    {
+                        _stringPlaintext.Insert(i + 1, "X");
+                    }
+
+                }
+            }
+
+            // độ dài plaintext là lẻ thì thêm x ở cuối 
+            else
+            {
+                _stringPlaintext.Append("X");
+            }
+
+            // xoá kí tự trùng trong key
+            StringBuilder _stringKey = new StringBuilder(key);
+
+            for (int i = 0; i < _stringKey.Length; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (_stringKey[i] == _stringKey[j])
+                    {
+                        _stringKey = _stringKey.Remove(i, 1);
+                        i--;
+                        break;
+                    }
+                }
+            }
+
+
+            // chèn key và bảng chữ cái vào trong ma trận.
+
+            char[,] matrix = new char[5, 5];
+            int k = 0;
+            KeyAddAlpa(_stringKey, alpa);
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    matrix[i, j] = _stringKey[k++];
+                }
+            }
+
+            // mã hoá 
+            k = 0;
+            StringBuilder result = new StringBuilder();
+            var arr = new List<int>(_stringPlaintext.Length * 2);
+            for (int i = 0; i < _stringPlaintext.Length * 2; i++)
+            {   
+                isCharacter(_stringPlaintext, i, matrix, arr);
+                
+            }
+
+
+            StringBuilder tmp = new StringBuilder();
+            for (int i = 0; i < _stringPlaintext.Length; i++)
+            {
+                tmp.Append(arr[i].ToString());
+                
+            }
+            rtbCiphtertext.Text = tmp.ToString();
+
+
+        }
+    }
+}
